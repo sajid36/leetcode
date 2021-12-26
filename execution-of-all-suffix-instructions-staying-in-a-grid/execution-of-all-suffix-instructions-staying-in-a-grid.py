00b0bb487eb701de14281e2dd93a64a67bbc6004
sajid36 <sajidulislam52@yahoo.com>
2021-12-26 01:51:19 -0500

    Time: 1276 ms (50.00%), Space: 14.2 MB (100.00%) - LeetHub

--- execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.py
+++ execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -1,14 +1,37 @@
 class Solution:
     def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
-        d = {'R': (0, 1), 'D': (1, 0), 'L': (0, -1), 'U': (-1, 0)}
-        ans = [0] * len(s)
-        for i in range(len(s)):
-            [x, y] = startPos
-            for j in range(i, len(s)):
-                x += d[s[j]][0]
-                y += d[s[j]][1]
-                if 0 <= x < n and 0 <= y < n:
-                    ans[i] += 1
-                else:
-                    break
-        return ans
+        initx = startPos[0]
+        inity = startPos[1]
+        answer = [0] * len(s)
+
+        for i in range (len(s)):
+            tempy, tempx = inity, initx
+            counter = 0
+            for j in range (i, len(s)):
+                if(s[j]=='R'):
+                    tempy = tempy + 1
+                    if(tempy > n - 1):
+                        break
+                elif(s[j]=='L'):
+                    tempy = tempy - 1
+                    if(tempy < 0):
+                        break
+                elif(s[j]=='D'):
+                    tempx = tempx + 1
+                    if(tempx > n - 1):
+                        break
+                elif(s[j]=='U'):
+                    tempx = tempx - 1
+                    if(tempx < 0):
+                        break
+
+                counter = counter + 1
+
+
+            answer[i] = counter
+
+
+
+
+        return (answer)
+        
