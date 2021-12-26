ff94e52d4108e1896f192b2bd4cfe264005530ee
sajid36 <sajidulislam52@yahoo.com>
2021-12-25 19:45:42 -0500

    Time: 28 ms (86.29%), Space: 14.2 MB (46.93%) - LeetHub

---
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        if (len(set(sentence)))==26:
+            return True
+        else:
+            return False
+        
