6395cb0f29522a75c6257781f28e14ea77d3c544
sajid36 <sajidulislam52@yahoo.com>
2021-12-25 23:58:17 -0500

    Time: 1219 ms (66.67%), Space: 14.6 MB (50.00%) - LeetHub

---
+++ execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -0,0 +1,56 @@
+class Solution:
+    def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
+        initx = startPos[0]
+        inity = startPos[1]
+        answer = [0] * len(s)
+
+        for i in range (len(s)):
+            tempy, tempx = inity, initx
+            counter = 0
+            if(s[i]=='R'):
+                tempy = tempy + 1
+                if(tempy > n - 1):
+                    continue
+
+            elif(s[i]=='L'):
+                tempy = tempy - 1
+                if(tempy < 0):
+                    continue
+            elif(s[i]=='D'):
+                tempx = tempx + 1
+                if(tempx > n - 1):
+                    continue
+            elif(s[i]=='U'):
+                tempx = tempx - 1
+                if(tempx < 0):
+                    continue
+
+            counter = counter + 1
+            for j in range (i+1, len(s)):
+                if(s[j]=='R'):
+                    tempy = tempy + 1
+                    if(tempy > n - 1):
+                        break
+                elif(s[j]=='L'):
+                    tempy = tempy - 1
+                    if(tempy < 0):
+                        break
+                elif(s[j]=='D'):
+                    tempx = tempx + 1
+                    if(tempx > n - 1):
+                        break
+                elif(s[j]=='U'):
+                    tempx = tempx - 1
+                    if(tempx < 0):
+                        break
+
+                counter = counter + 1
+
+
+            answer[i] = counter
+
+
+
+
+        return (answer)
+        
