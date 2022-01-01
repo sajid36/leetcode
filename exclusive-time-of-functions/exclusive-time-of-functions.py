05ae609323089a04f7c115090bb892ca70292e87
sajid36 <sajidulislam52@yahoo.com>
2021-12-31 22:36:09 -0500

    Time: 152 ms (5.03%), Space: 14.5 MB (58.07%) - LeetHub

--- exclusive-time-of-functions/exclusive-time-of-functions.py
+++ exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,50 +1,22 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        
-        
-        task = defaultdict(list)
-        prevTaskId = -1
-        prevTime = -1
-        active = []
-        for i in range (len(logs)-1):
-            parts = logs[i].split(':')
-            task1 = parts[0]
-            status1 = parts[1]
-            time1 = parts[2]
-            
-            parts = logs[i+1].split(':')
-            task2 = parts[0]
-            status2 = parts[1]
-            time2 = parts[2]
-            
-            
-            if status1=="start" and status2=="start":
-                task[task1].append(int(time2) - int(time1))
-                active.append(task1)
-                
-            elif status1=="start" and status2=="end":
-                if(task1==task2):
-                    task[task1].append(int(time2) - int(time1)+1)
-                    
-            elif status1=="end" and status2=="end":
-                if(task1==task2):
-                    task[task1].append(int(time2) - int(time1))
-                    active.pop()
-                    
-                else:
-                    task[task2].append(int(time2) - int(time1))
-                    active.pop()
-            
-            elif status1=="end" and status2=="start":
-                if len(active) > 0:
-                    act = active.pop()
-                    task[act].append(int(time2) - int(time1) - 1)
-                    active.append(act)  
+        ans = [0] * n
+        stack = []
+        prev_time = 0
 
-        res = [0]*n
-        for key, val in task.items():
-            res[int(key)] = sum(val)
-                                    
-        return (res)
+        for log in logs:
+            fn, typ, time = log.split(':')
+            fn, time = int(fn), int(time)
+
+            if typ == 'start':
+                if stack:
+                    ans[stack[-1]] += time - prev_time 
+                stack.append(fn)
+                prev_time = time
+            else:
+                ans[stack.pop()] += time - prev_time + 1
+                prev_time = time + 1
+
+        return ans
             
             
