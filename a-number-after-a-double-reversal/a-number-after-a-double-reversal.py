f7616c290e48d488c7659dc8ffa8ee6d445ad752
sajid36 <sajidulislam52@yahoo.com>
2022-01-06 03:40:09 -0500

    Delete a-number-after-a-double-reversal directory

--- a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isSameAfterReversals(self, num: int) -> bool:
-        temp = str(num)
-
-        temp = str(int(temp[::-1]))
-        temp = int(temp[::-1])
-        
-        return num==temp
-        
