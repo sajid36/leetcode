18876a5cf1296f33ec723d71f9850ccc4ae323f5
sajid36 <sajidulislam52@yahoo.com>
2022-01-23 16:40:57 -0500

    Time: 28 ms (97.10%), Space: 14.3 MB (30.28%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,28 +4,16 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        
         n = len(matrix)
-        
         res = defaultdict(list)
         
         for i in range (n):
             item = []
             for j in range (n):
                 item.append(matrix[j][i])
-                
             res[i].append(item[::-1])
         
         
         for i in range (n):
             for j in range (n):
                 matrix[i][j] = res[i][0][j]
-                
-                
-                
-        
-        
-        
-            
-            
-         
